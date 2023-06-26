d24b3f14c4284f2acb4928ae4cf5dd27c592868c
Swikar Adhikari <swikara082@gmail.com>
2023-06-27 08:08:00 +0930

    fix: 🐛 some bugs in layout

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -26,25 +26,23 @@ const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
               image: session?.user.image || null,
             }}
           />
-          <span className="absolute bottom-0 righ-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
-        </div>
 
+          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white" />
+        </div>
         <Input
-          readOnly
           onClick={() => router.push(pathname + "/submit")}
+          readOnly
           placeholder="Create post"
         />
-
         <Button
-          variant="ghost"
           onClick={() => router.push(pathname + "/submit")}
+          variant="ghost"
         >
           <ImageIcon className="text-zinc-600" />
         </Button>
-
         <Button
-          variant="ghost"
           onClick={() => router.push(pathname + "/submit")}
+          variant="ghost"
         >
           <Link2 className="text-zinc-600" />
         </Button>
