8f1760fa460cece8208a59804c208f6f71e93542
Swikar Adhikari <swikara082@gmail.com>
2023-06-27 09:44:09 +0930

    feat: 💭 creating post logic is done

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -32,7 +32,7 @@ const page = async ({ params }: PageProps) => {
       </div>
 
       {/* form */}
-      <Editor />
+      <Editor subredditId={subreddit.id} />
 
       <div className="w-full flex justify-end">
         <Button type="submit" className="w-full" form="subreddit-post-form">
