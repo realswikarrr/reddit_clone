cce3a71e9c9449b5b9aceddd7acdb0c3c0d001dd
Swikar Adhikari <swikara082@gmail.com>
2023-06-27 13:15:00 +0930

    feat: 💭 implemented logic to display contents posted through editor

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,6 +2,7 @@ import { formatTimeToNow } from "@/lib/utils";
 import { Post, User, Vote } from "@prisma/client";
 import { MessageSquare } from "lucide-react";
 import { FC, useRef } from "react";
+import EditorOutput from "./EditorOutput";
 
 interface PostProps {
   subredditName: string;
@@ -48,6 +49,7 @@ const Post: FC<PostProps> = ({ subredditName, post, commentAmt }) => {
             className="relative text-sm max-h-40 w-full overflow-clip"
             ref={pRef}
           >
+            <EditorOutput content={post.content} />
             {pRef.current?.clientHeight === 160 ? (
               <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent " />
             ) : null}
