e394edacb03e9c7e5f25a5d5b6a60b257606a84c
Swikar Adhikari <swikara082@gmail.com>
2023-07-03 08:55:52 +0930

    feat: 💭 added reply functionality / also comment voting functionality

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { FC, useRef } from "react";
+import { FC, useRef, useState } from "react";
 import UserAvatar from "./UserAvatar";
 import { Comment, CommentVote, User } from "@prisma/client";
 import { formatTimeToNow } from "@/lib/utils";
+import CommentVotes from "./CommentVotes";
+import { Button } from "./ui/Button";
+import { MessageSquare } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { Label } from "./ui/Label";
+// @ts-ignore
+import { Textarea } from "./ui/textarea";
+import { useMutation } from "@tanstack/react-query";
+import { CommentRequest } from "@/lib/validators/comment";
+import axios from "axios";
+import { toast } from "@/hooks/use-toast";
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -12,10 +24,51 @@ type ExtendedComment = Comment & {
 
 interface PostCommentProps {
   comment: ExtendedComment;
+  votesAmt: number;
+  currentVote: CommentVote | undefined;
+  postId: string;
 }
 
-const PostComment: FC<PostCommentProps> = ({ comment }) => {
+const PostComment: FC<PostCommentProps> = ({
+  comment,
+  votesAmt,
+  currentVote,
+  postId,
+}) => {
   const commentRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [isReplying, setIsReplying] = useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+
+  const { mutate: postComment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = {
+        postId,
+        text,
+        replyToId,
+      };
+
+      const { data } = await axios.patch(
+        "/api/subreddit/post/comment",
+        payload
+      );
+      return data;
+    },
+    onError: () => {
+      return toast({
+        title: "Something Went Wrong",
+        description: "Comment wasnt posted sucessfully please try again",
+        variant: "destructive",
+      });
+    },
+
+    onSuccess: () => {
+      router.refresh();
+      setIsReplying(false);
+      setInput("");
+    },
+  });
 
   return (
     <div ref={commentRef} className="flex flex-col">
@@ -39,6 +92,64 @@ const PostComment: FC<PostCommentProps> = ({ comment }) => {
       </div>
 
       <p className="text-sm text-zinc-900 mt-2 ">{comment.text}</p>
+
+      <div className="flex gap-2 items-center flex-wrap">
+        <CommentVotes
+          commentId={comment.id}
+          initialVotesAmt={votesAmt}
+          initialVote={currentVote}
+        />
+
+        <Button
+          onClick={() => {
+            if (!session) return router.push("/sign-in");
+            setIsReplying(true);
+          }}
+          variant="ghost"
+          size="xs"
+        >
+          <MessageSquare className="h-4 w-4 mr-1.5" /> Reply
+        </Button>
+
+        {isReplying ? (
+          <div className="grid w-full gap-1.5s">
+            <Label htmlFor="comment">Your Comment </Label>
+            <div className="mt-2">
+              <Textarea
+                id="comment"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                rows={1}
+                placeholder="What are your thoughts ?"
+              />
+
+              <div className="mt-2 flex justify-end gap-2">
+                <Button
+                  tabIndex={-1}
+                  variant="subtle"
+                  onClick={() => setIsReplying(false)}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  isLoading={isLoading}
+                  disabled={input.length === 0}
+                  onClick={() => {
+                    if (!input) return;
+                    postComment({
+                      postId,
+                      text: input,
+                      replyToId: comment.replyToId ?? comment.id,
+                    });
+                  }}
+                >
+                  Post
+                </Button>
+              </div>
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 };
