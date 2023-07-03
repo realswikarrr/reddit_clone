70d8eeaf5c53a586a0b29cc84a14584ba4dbcff7
Swikar Adhikari <swikara082@gmail.com>
2023-07-03 10:07:55 +0930

    feat: 💭 really cool search functionality added / debounce used loadash

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import { Icons } from "./icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import UserAccountNav from "./UserAccountNav";
+import SearchBar from "./SearchBar";
 
 const Navbar = async () => {
   const session = await getAuthSession();
@@ -20,6 +21,7 @@ const Navbar = async () => {
         </Link>
 
         {/* Search Bar */}
+        <SearchBar />
 
         {session?.user ? (
           <UserAccountNav user={session.user} />
